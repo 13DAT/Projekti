40b983b3172cb4d4fa8d91950bef1a965d392202
joriheikkila <jori.heikkila@kpedu.fi>
2015-09-18 12:32:57 +0300

    testi

---
+++ vasenreuna.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>jQuery UI Sortable - Drop placeholder</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+  #sortable { list-style-type: none; margin: 0; padding: 0; width: 20%; }
+  #kaa{width:100px; height:100px;}
+  #sortable li { margin: 0 5px 5px 5px; padding: 5px; font-size: 1.2em; height: 1.5em; }
+  html>body #sortable li { height: 10em; line-height: 1.2em; }
+   .ui-state-highlight { height: 1.5em; line-height: 1.2em; }
+  </style>
+  <script>
+  $(function() {
+    $( "#sortable" ).sortable({
+      placeholder: "ui-state-highlight"
+    });
+    $( "#sortable" ).disableSelection();
+  });
+  </script>
+</head>
+<body>
+ 
+<ul id="sortable">
+  <li class="ui-state-default">Kuva</li>
+  <li class="ui-state-default">Kuva</li>
+  <li class="ui-state-default">Kuva</li>
+  <li class="ui-state-default">Kuva</li>
+</ul>
+ 
+ 
+</body>
+</html>
