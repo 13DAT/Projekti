43f73c8b449ea9884f126914851cf06c512196bd
erkkasailynoja <erkka.sailynoja@kpedu.fi>
2015-09-21 09:21:15 +0300

    koodia

--- fullscreen/fullscreen.php
+++ fullscreen/fullscreen.php
@@ -51,3 +51,29 @@
     });
   </script>
 </head>
+  <body>
+  <h1>jQuery.fullsizable</h1>
+  <div id="container">
+    <a href="http://i.imgur.com/LVYDgNw.jpg">
+      <img title="Huorra"
+            alt="Steam locomotives of the Chicago &amp; North Western Railway."
+            src="http://i.imgur.com/LVYDgNw.jpg">
+    </a>
+    <a href="http://cdni.condenast.co.uk/813x494/a_c/Austin-Powers-GQ-05Dec14_rex_b_813x494.jpg">
+      <img title="Huorra"
+            alt="”The debris loading isn't particularly extensive, but the color is usual.”"
+            src="http://cdni.condenast.co.uk/813x494/a_c/Austin-Powers-GQ-05Dec14_rex_b_813x494.jpg">
+    </a>
+    <a href="http://2.bp.blogspot.com/-H2lbv15f2X8/VEqIvDoAa5I/AAAAAAAAAEc/5RPIicE4KBM/s1600/Lisa-Ann-TheLisaAnn.com_.jpg">
+      <img title="Huorra"
+            alt="Xysticus cristatus, A Estrada, Galicia, Spain"
+            src="http://2.bp.blogspot.com/-H2lbv15f2X8/VEqIvDoAa5I/AAAAAAAAAEc/5RPIicE4KBM/s1600/Lisa-Ann-TheLisaAnn.com_.jpg">
+    </a>
+    <a href="https://lh3.googleusercontent.com/-49LWhpN0s3k/VOiQYP51bDI/AAAAAAAAANQ/Je0mO5u7efI/s630-fcrop64=1,00420000ffbcd5a6/10378206_791478980944361_672948458537634199_n_resized.png">
+      <img title="Huorra"
+            alt="Xysticus cristatus, A Estrada, Galicia, Spain"
+            src="https://lh3.googleusercontent.com/-49LWhpN0s3k/VOiQYP51bDI/AAAAAAAAANQ/Je0mO5u7efI/s630-fcrop64=1,00420000ffbcd5a6/10378206_791478980944361_
+    </a>
+  </div>
+</body>
+</html>
