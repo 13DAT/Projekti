9c15218fe3197f55f00c4590756a70b2c0fdd82b
anttitaipale <antti.taipale@kpedu.fi>
2015-09-28 10:20:11 +0300

    heps

--- upload.php
+++ upload.php
@@ -14,6 +14,7 @@
 
 
 
+    
     $yhteys=new mysqli("localhost","data14","mv2Mqbm888DvqbjT","data14");
     if(mysqli_connect_errno()) {
         die("MySQL, virhe yhteyden luonnissa:" . mysqli_connect_error());
@@ -23,32 +24,34 @@
 
 
 
-                    $asia = $yhteys->real_escape_string(trim(htmlentities(strip_tags($_POST['MAX_FILE_SIZE']))));
+                    
                     if(!empty($_FILES)){
-                        $targetDir = "/home/jannenyman/public_html/projekti/Projekti/kuvat/";
+                        $targetDir = "/home/anttitaipale/public_html/projekti/Projekti/kuvat/";
                         $fileName = $_FILES['file']['name'];
-                        $targetFileName = md5(microtime());
+                        $targetFileName = md5(microtime()).'.jpg';
 
-                    if(move_uploaded_file($_FILES['file']['tmp_name'],$targetDir.$targetFileName)){
-                    echo "Lataus onnistui\n";
-                     $sql = ("INSERT INTO projekti_kuvat (md5, nimi) VALUES('$targetFileName','$fileName');
-                              UPDATE projekti_äänet           
+                        if(move_uploaded_file($_FILES['file']['tmp_name'],$targetDir.$targetFileName)){
+                            echo "Lataus onnistui\n";
+                            $sql = ("INSERT INTO projekti_kuvat (md5_nimi, nimi) VALUES('$targetFileName','$fileName');
+                              UPDATE projekti_kuvat           
                               WHERE id = ID;;");
+                        } else {
+                            echo " ";
+                        }
                     } else {
-                        echo "\n";
-                    }
+                        echo " ";
                     }
 
                     if($tulos=$yhteys->multi_query($sql)) {
                         echo " ";}
                     else {
-                        echo " " . " " . $sql . " " . $yhteys->error;
+                        echo " ";
                     }
-/*    echo '<pre>';
-    var_export($GLOBALS);
+   # echo '<pre>';
+    #var_export($GLOBALS);
     
-    echo '</pre>';
-*/
+    #echo '</pre>';
+
     $yhteys->close();
 
 
@@ -56,8 +59,7 @@
 ?>
 
 </center>
-    <p>$last_line = system('convert '.$targetFile.' -gamma .45455 -resize 720x700 -crop 720x700+10+10 -resize 750x700  -gamma 2.2  \
-        -quality 92 '.$targetFile.'', $retval);</p>
+
 </body>
 <script src="http://cosmo.kpedu.fi/~jannenyman/testi/proto/dropzone.js"></script>
 </html>
