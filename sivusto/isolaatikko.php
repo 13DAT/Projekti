833f275ceccfafbadb069e91c0fda6305b24fc7e
jonatanlagland <jonatan.lagland@kpedu.fi>
2015-09-28 09:16:24 +0300

    juupajuu

--- sivusto/isolaatikko.php
+++ sivusto/isolaatikko.php
@@ -8,12 +8,12 @@
 
     <body>
 <!-- dropzone laatikot  -->
-<ul id="boxi">
-  <li class="ui-priority-primary">
+<div id="laatikko">
+  <div class="ui-priority-primary">
       <form action="/file-upload" class="dropzone dz-clickable displayed" id="kaa">
       </form>
-  </li>
-</ul>
+  </div>
+</div>
     </body>
 <!-- estää kuvien stackaamisen "poistamalla" edellisen kuvan -->
      <script src="http://cosmo.kpedu.fi/~jonatanlogland/projekti/Projekti/sivusto/dropzone.js"></script>
@@ -31,18 +31,6 @@
             });
         }
     };
-     Dropzone.options.kab = {
-        accept: function(file, done) {
-            console.log("uploaded");
-            done();
-        },
-        init:function() {
-            this.on("addedfile", function(){
-                if (this.files[1]!=null){
-                    this.removeFile(this.files[0]);
-                }
-            });
-        }
-    };
+     
 
 </script>
